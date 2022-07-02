89edb47f8dda4ab0f9b26f2eeca38935ed3c98ae
DiMiller <dima.lux.estate@gmail.com>
2022-07-02 17:22:59 +0300

    commit-6
    mob-menu

--- js/mob-menu.js
+++ js/mob-menu.js
@@ -1,16 +1,16 @@
 (() => {
-    const refs = {
-      openMenuBtn: document.querySelector('.menu-open-btn'),
-      closeMenuBtn: document.querySelector('.menu-close-btn'),
-      menu: document.querySelector('.mob-menu'),
-      body: document.querySelector('body'),
-    };
- 
-    refs.openMenuBtn.addEventListener('click', toggleMenu);
-    refs.closeMenuBtn.addEventListener('click', toggleMenu);
- 
-    function toggleMenu() {
-      refs.menu.classList.toggle('is-hidden');
-      refs.body.classList.toggle('no-scroll');
-    }
-  })();
+   const refs = {
+     openMobmenuBtn: document.querySelector('.mob-open-btn'),
+     closeMobmenuBtn: document.querySelector('.mob-close-btn'),
+     Mobmenu: document.querySelector('.mobile-menu'),
+     body: document.querySelector('body'),
+   };
+
+   refs.openMobmenuBtn.addEventListener('click', toggleMobmenu);
+   refs.closeMobmenuBtn.addEventListener('click', toggleMobmenu);
+
+   function toggleMobmenu() {
+     refs.Mobmenu.classList.toggle('is-hidden');
+     refs.body.classList.toggle('no-scroll');
+   }
+ })();
